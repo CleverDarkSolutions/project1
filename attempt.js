7c5bb997113772b96af4aa8f72d8dd51643dfc07
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2020-12-14 08:55:34 +0100

    zmiany lul

--- attempt.js
+++ attempt.js
@@ -3,7 +3,11 @@ let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let canvas = renderer.domElement;
+canvas.style.position = "absolute";
+canvas.style.top = "0px";
+canvas.style.zIndex = "-1";
+document.getElementsByClassName('wrapper')[0].appendChild(canvas);
 
 
 window.addEventListener('resize', function () {
@@ -52,30 +56,36 @@ let textWrapper6 = document.querySelector('#elementFour');
 textWrapper6.innerHTML = textWrapper6.textContent.replace(/\S/g, "<span class='letter spanFour'>$&</span>");
 
 function textAnimation3() {
-    anime.timeline({ loop: 1 })
+    anime.timeline({
+            loop: 1
+        })
         .add({
             targets: '.mainContent .letter',
             opacity: [0, 1],
             easing: "easeInOutQuad",
             duration: 2250,
-            delay: (el, i) => 50* (i + 1)
+            delay: (el, i) => 50 * (i + 1)
         })
 }
 
 function textAnimation2() {
-    anime.timeline({ loop: 1 })
+    anime.timeline({
+            loop: 1
+        })
         .add({
             targets: '.navElement .letter',
             opacity: [0, 1],
             easing: "easeInOutQuad",
             duration: 2250,
-            delay: (el, i) => 50* (i + 1)
+            delay: (el, i) => 50 * (i + 1)
         })
 }
 
 
 function textAnimation() {
-    anime.timeline({ loop: 1 })
+    anime.timeline({
+            loop: 1
+        })
         .add({
             targets: '.welcomeText .letter',
             opacity: [0, 1],
@@ -85,7 +95,7 @@ function textAnimation() {
         })
 }
 
-function hoverSelectedMenu(item){
+function hoverSelectedMenu(item) {
     // DO POPRAWY
     document.getElementById("elementOne").style.fontSize = "30";
     document.getElementById("elementTwo").style.fontSize = "30";
@@ -96,17 +106,17 @@ function hoverSelectedMenu(item){
 
 //-----------------TEXT---------------------------
 
-class contentObj{
-    constructor(text,number){
+class contentObj {
+    constructor(text, number) {
         this.text = text;
         this.number = number;
     }
 };
 
-let subPage1 = new contentObj("strona1",1);
-let subPage2 = new contentObj("strona2",2);
-let subPage3 = new contentObj(mainContent,3);
-let subPage4 = new contentObj("strona4",4);
+let subPage1 = new contentObj("strona1", 1);
+let subPage2 = new contentObj("strona2", 2);
+let subPage3 = new contentObj(mainContent, 3);
+let subPage4 = new contentObj("strona4", 4);
 let currentPage = 0;
 
 function loadPage(content) {
@@ -128,7 +138,12 @@ function loadPage(content) {
 
 }
 
-//-------------------------------------------------------
+//-----------------CSS-PROPERTIES-BO-NIE-DZIAŁA---------------
+
+document.getElementById("mainContent").className += " slideSideways";
+document.getElementById("secondScene").className += " slideSideways";
+
+//------------------------------------------------------------
 
 let counter = 0;
 let interval1;
@@ -137,42 +152,42 @@ function accelerate() {
     if (camera.position.z < 12.5) {
         counter++;
         camera.position.z += 0.0175;
-        console.log("acc1");
     }
-    if(camera.position.z > 12.4){
+    if (camera.position.z > 12.4) {
         clearInterval(interval1);
     }
 }
 
-let nextRng = 1;
-/*
-function accelerate2(rng){
- 
-        if(rng == 1){
-            if(camera.position.z > 10){
-                camera.position.z -= 0.02;
-                camera.position.y += 0.02;
-                camera.position.x += 0.02;
-                console.log("1");
-            }
+let testvar1 = 13;
+let interval2;
+
+function test() {
+    document.querySelectorAll('.slideSideways').forEach(function (item) {
+        if (testvar1 < 200) {
+            let value = testvar1;
+            item.style.left = value + "vw";
+            testvar1 *= 1.01;
+        } else {
+            document.getElementById("secondScene").style.left = 13 + "vw";
+            document.getElementById("mainContent").style.left = 13 + "vw";
+            clearInterval(interval2);
         }
-        
-        if(rng == 2){
-            if(camera.position.z > 12){
-                camera.position.z -= 0.02;
-                console.log("1");
-            }
-        }
+
+    })
 }
-*/
+
 
 
 let dotArray = [];
+
 function addDots(n) {
 
     for (var j = 0; j < n; j++) {
         const dotGeometry = new THREE.SphereGeometry(0.01, 12, 12);
-        const dotMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide });
+        const dotMaterial = new THREE.MeshBasicMaterial({
+            color: 0xFFFFFF,
+            side: THREE.DoubleSide
+        });
         const dot = new THREE.Mesh(dotGeometry, dotMaterial);
         scene.add(dot);
         dotArray.push(dot);
@@ -192,7 +207,7 @@ function updateDots() {
 
         let randomNum1 = Math.random() * 15;
         let randomNum2 = Math.random() * 10;
-         let randomNum3 = (Math.random() * 5) + 10;
+        let randomNum3 = (Math.random() * 5) + 10;
 
         if (array[i].position.x > 20 || array[i].position.y > 15 || array[i].position.z < 0) {
             array[i].position.x = randomNum1;
@@ -205,63 +220,72 @@ function updateDots() {
 
 //----TIMEOUTS--------------------------
 
-setTimeout(function(){
-    setTimeout(function () { document.getElementById("welcomeText").style.display = "block" }, 500); // zapobieganie przedwczesnemu zaladowaniu sie animacji
+setTimeout(function () {
+    setTimeout(function () {
+        document.getElementById("welcomeText").style.display = "block"
+    }, 500); // zapobieganie przedwczesnemu zaladowaniu sie animacji
     textAnimation();
-},100);
+}, 100);
 
-setTimeout(function(){
-    interval1 = setInterval(function () { accelerate() }, 5);
+setTimeout(function () {
+    interval1 = setInterval(function () {
+        accelerate()
+    }, 5);
     textAnimation2();
     textAnimation3();
     document.getElementById("welcomeText").style.display = "none";
- }, 7000);
+}, 7000);
 
- setTimeout(function(){
+setTimeout(function () {
     document.getElementById("secondScene").style.display = "block";
     document.getElementById("mainContent").style.display = "block";
- }, 7500);
+}, 7500);
 
- //--------------ONCLICKS------------
+setTimeout(function () {
+    interval2 = setInterval(function () {
+        test();
+    }, 5);
+}, 12000);
+//--------------ONCLICKS------------
 
- document.querySelectorAll('.spanOne').forEach(function(item) {
-     item.addEventListener('click', function() {
+document.querySelectorAll('.spanOne').forEach(function (item) {
+    item.addEventListener('click', function () {
         loadPage(subPage1);
- //       setInterval(function(){accelerate2(1)},10);
-     })
- })
-
- document.querySelectorAll('.spanTwo').forEach(function(item) {
-    item.addEventListener('click', function() {
-       loadPage(subPage2);
+        //       setInterval(function(){accelerate2(1)},10);
     })
 })
 
-document.querySelectorAll('.spanThree').forEach(function(item) {
-    item.addEventListener('click', function() {
-       loadPage(subPage3);
+document.querySelectorAll('.spanTwo').forEach(function (item) {
+    item.addEventListener('click', function () {
+        loadPage(subPage2);
     })
 })
 
-document.querySelectorAll('.spanFour').forEach(function(item) {
-    item.addEventListener('click', function() {
-       loadPage(subPage4);
+document.querySelectorAll('.spanThree').forEach(function (item) {
+    item.addEventListener('click', function () {
+        loadPage(subPage3);
     })
 })
 
-document.getElementById("elementOne").addEventListener("click",function(){
+document.querySelectorAll('.spanFour').forEach(function (item) {
+    item.addEventListener('click', function () {
+        loadPage(subPage4);
+    })
+})
+
+document.getElementById("elementOne").addEventListener("click", function () {
     hoverSelectedMenu("elementOne");
 });
 
-document.getElementById("elementTwo").addEventListener("click",function(){
+document.getElementById("elementTwo").addEventListener("click", function () {
     hoverSelectedMenu("elementTwo");
 });
 
-document.getElementById("elementThree").addEventListener("click",function(){
+document.getElementById("elementThree").addEventListener("click", function () {
     hoverSelectedMenu("elementThree");
 });
 
-document.getElementById("elementFour").addEventListener("click",function(){
+document.getElementById("elementFour").addEventListener("click", function () {
     hoverSelectedMenu("elementFour");
 });
 
